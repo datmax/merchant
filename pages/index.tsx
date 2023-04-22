4b4e797b5581291f7cc323c022b2c4a82c345f66
0xtokendeployer <shitdeployer@proton.me>
2023-04-22 08:08:40 +0200

    added meta + hover

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import { FaStarOfDavid } from 'react-icons/fa';
+import Link from 'next/link';
+import { FaStarOfDavid, FaTwitter } from 'react-icons/fa';
 const text =
   'מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃ מוֹדֶה אֲנִי לְפָנֶֽיךָ מֶֽלֶךְ חַי וְקַיָּים. שֶׁהֶֽחֱזַֽרְתָּ בִּי נִשְׁמָתִי ,בְּחֶמְלָה. רַבָּה אֱמֽוּנָתֶֽךָ׃';
 
@@ -40,30 +41,50 @@ export default function Home() {
         $SHALOM is the rest day of Jewish. It happens each week from sunset on
         Friday to sunset on Saturday.
       </div>
+      <div className="flex justify-center my-2">
+        <Link href="https://twitter.com/ThaMerchant">
+          <FaTwitter size={24}></FaTwitter>
+        </Link>
+      </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-2 px-4 lg:px-32 mt-10 lg:mt-20">
         <div className="flex flex-col gap-y-6">
           <div className="text-2xl w-full text-black">TOKENOMICS:</div>
-          <div className="flex w-full align-baseline">
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            className="flex w-full align-baseline"
+          >
             <FaStarOfDavid size={24}></FaStarOfDavid>
             Supply : 77.777.777.777
-          </div>
-          <div className="flex w-full align-baseline">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            className="flex w-full align-baseline"
+          >
             <FaStarOfDavid size={24}></FaStarOfDavid>
             Presale : 5%
-          </div>
-          <div className="flex w-full align-baseline">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            className="flex w-full align-baseline"
+          >
             <FaStarOfDavid size={24}></FaStarOfDavid>
             LP Locked for ever
-          </div>
-          <div className="flex w-full align-baseline">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            className="flex w-full align-baseline"
+          >
             <FaStarOfDavid size={24}></FaStarOfDavid>
             Tax : 3% Buy 5% Sell
-          </div>
-          <div className="flex w-full align-baseline">
+          </motion.div>
+          <motion.div
+            whileHover={{ scale: 1.01 }}
+            className="flex w-full align-baseline"
+          >
             <FaStarOfDavid size={24}></FaStarOfDavid>
             Team: 3%
-          </div>
+          </motion.div>
         </div>
         <img src="shabbat.png" className="w-full"></img>
       </div>
