e1f9d2149aca01db13fb5fdcfbfd986152f07c9c
Max bibi <unrusso@live.com>
2023-04-22 05:44:26 +0200

    heheheh merch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Urbanist } from 'next/font/google';
+const inter = Urbanist({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
