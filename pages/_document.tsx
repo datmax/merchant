773e97bfe10b2f305c7d59c259aae91da9363b87
0xtokendeployer <shitdeployer@proton.me>
2023-04-22 08:02:07 +0200

    added meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,22 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          name="description"
+          content="The best coin. We make you rich. Buy now."
+        />
+        <meta property="og:title" content="THE merchant coin." />
+
+        <meta property="og:image" content="/shabbat.png" />
+        <meta />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
